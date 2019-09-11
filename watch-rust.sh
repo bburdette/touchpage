d33e0e6253ea85bd2e75efe8df4f200e30354daa
Ben Burdette <bburdette@gmail.com>
2019-09-10 20:47:41 -0600

    re-fix watch-rust

--- watch-rust.sh
+++ watch-rust.sh
@@ -2,7 +2,8 @@
 
 
 # 'reflex' program is required for this.
-reflex -r 'src/' -s -- sh -c 'clear && cargo build'
+# reflex -r '^./src/' -s -- sh -c 'clear && cargo build'
+reflex -g 'src/*.rs' -s -- sh -c 'clear && cargo build'
 
 
 

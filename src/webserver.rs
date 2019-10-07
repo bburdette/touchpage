cccd62058d7fe3bf69abf51bcd59dfb8616aacf9
Ben Burdette <bburdette@gmail.com>
2019-10-07 14:31:53 -0600

    block parameter for web server

--- src/webserver.rs
+++ src/webserver.rs
@@ -2,9 +2,34 @@ use actix_web::fs::NamedFile;
 use actix_web::http::{Method, StatusCode};
 use actix_web::{server, App, HttpRequest, HttpResponse, Result};
 use std::path::{Path, PathBuf};
+use std::thread;
 use string_defaults;
 
 pub fn startwebserver<'a>(
+  ip: &str,
+  http_port: &str,
+  websockets_port: &str,
+  htmltemplatefile: Option<String>,
+  block: bool,
+) {
+  if block {
+    startwebserver_impl(ip, http_port, websockets_port, htmltemplatefile);
+  } else {
+    // Spawn a thread for the server.
+    let ip = ip.to_string();
+    let http_port = http_port.to_string();
+    let websockets_port = websockets_port.to_string();
+    thread::spawn(move || {
+      startwebserver_impl(
+        ip.as_str(),
+        http_port.as_str(),
+        websockets_port.as_str(),
+        htmltemplatefile,
+      );
+    });
+  }
+}
+pub fn startwebserver_impl<'a>(
   ip: &str,
   http_port: &str,
   websockets_port: &str,
